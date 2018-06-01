9c8eb7cdf9d8729f0c00d2f58e0b9d42159c54d5
kingkadienm <1325008026@qq.com>
2018-06-01 17:50:01 +0800

    BaseRecyclerView

---
+++ plugin_common/src/main/java/com/example/admin/plugin_common/recyclerview/entity/MultiItemEntity.java
@@ -0,0 +1,8 @@
+package com.example.admin.plugin_common.recyclerview.entity;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface MultiItemEntity {
+    int getItemType();
+}
